4145d85cc703a6f253cff5fd929213096530bc2e
jayanthkrishna <jayanth.krishna.g@gmail.com>
2023-07-26 22:46:16 +0530

    List deployments

--- main.go
+++ main.go
@@ -1,25 +1,53 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 
-	kubeconfig := flag.String("kubeconfig", "~/.kube/config", "location to your config file")
+	kubeconfig := flag.String("kubeconfig", "/home/eangrki/.kube/config", "location to your config file")
 
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+
+	if err != nil {
+		fmt.Print(err)
+	}
 
 	clientSet, err := kubernetes.NewForConfig(config)
 
 	if err != nil {
-		print(err)
+		fmt.Print(err)
 	}
 
-	fmt.Print(clientSet)
+	ctx := context.Background()
+	pods, err := clientSet.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
 
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Print("Pods from default namespace")
+
+	for _, i := range pods.Items {
+		fmt.Printf("%s\n", i.Name)
+	}
+
+	fmt.Print("Deployments from default namespace")
+
+	deployments, err := clientSet.AppsV1().Deployments("default").List(ctx, metav1.ListOptions{})
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	for _, i := range deployments.Items {
+		fmt.Printf("%s\n", i.Name)
+	}
 }
